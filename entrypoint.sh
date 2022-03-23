62e0365d4ef161397163199e9f623bfdcec0a046
Peter Njeim <peterthegreatpeterthegreat@hotmail.com>
2022-03-22 21:11:42 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -87,7 +87,8 @@ then
 fi
 
 echo "[+] Copying contents of source repository folder $SOURCE_DIRECTORY to folder $TARGET_DIRECTORY in git repo $DESTINATION_REPOSITORY_NAME"
-cp -ra "$SOURCE_DIRECTORY"/. "$CLONE_DIR/$TARGET_DIRECTORY"
+cd "$SOURCE_DIRECTORY"
+tar -cf - * | tar -xf - -C "$CLONE_DIR/$TARGET_DIRECTORY"
 cd "$CLONE_DIR"
 
 echo "[+] Files that will be pushed"
